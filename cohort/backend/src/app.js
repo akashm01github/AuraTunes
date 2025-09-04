11b3e6f61ec3d660f50ec144c4dc6b118cc31579
akashm01github <akashmukhopadhayay@gmail.com>
2025-09-04 17:01:04 +0530

    Backend Updated

--- cohort/backend/src/app.js
+++ cohort/backend/src/app.js
@@ -3,6 +3,10 @@ const songRoutes = require('./routes/song.routes');
 
 const cors = require('cors');
 
+const path = require("path");
+
+
+
 
 const app = express();
 
@@ -13,7 +17,12 @@ app.use(cors());
 
 app.use('/',songRoutes);
 
+app.use(express.static(path.join(__dirname,'../public')))
 
+
+app.get('*name',(req,res)=>{
+    res.sendFile(path.join(__dirname,'../public/index.html'));
+})
 
 
 module.exports = app;
